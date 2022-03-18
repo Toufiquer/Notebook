184ed6899f89faf2039556739bdb8b12d1979b48
Toufiquer Tahman <toufiquerabc@gmail.com>
2022-03-18 07:29:42 +0600

    js:

---
+++ javascript.js
@@ -0,0 +1,38 @@
+// JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997.
+// ECMA-262 is the official name of the standard. ECMAScript is the official name of the language.
+// Get the document
+document.getElementById("demo");
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById("demo").innerText = "Hello JavaScript";
+// Change the style
+document.getElementById("demo").style.fontSize = "35px";
+document.getElementById("demo").style.display = "none";
+document.getElementById("demo").style.display = "block";
+// For inside html
+/* <script> */
+document.getElementById("demo").innerHTML = "My First JavaScript";
+/* </script> */
+
+// JavaScript Output
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+// *******
+// Using document.write() after an HTML document is loaded, will delete all existing HTML:
+// for printing some document
+window.print();
+// JavaScript statements are composed of:
+// Values, Operators, Expressions, Keywords, and Comments.
+
+var x; /** Global Variable */
+let y; /** Not a Global Variable Only access inside {} */
+x = 2;
+y = 3;
+const z = 5;
+let person = "John Doe",
+    carName = "Volvo",
+    price = 200;
+
+// https://www.w3schools.com/js/js_let.asp
